d0406c4ee277cf9a7a10e6b617ef06b2724c4f19
VuDuong-sketch <errolne1309@gmail.com>
2025-03-31 11:04:08 +0700

    V

--- moveGame.cpp
+++ moveGame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <cmath>
 using namespace std;
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
@@ -8,10 +9,11 @@ void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 
 void waitUntilPressed();
 
-char a[100][100];
+char a[100][100], x[100][100];
 int m, n;
 SDL_Event e;
 bool flag;
+const int K = 100;
 
 struct coordinates {
 	int x, y;
@@ -21,7 +23,7 @@ struct coordinates {
 	}
 };
 
-coordinates M(1,1), B(2,2);
+coordinates M(1,1);
 
 void printArr() {
 	for(int i = 0; i < m + 2; i++) {
@@ -46,7 +48,7 @@ void Move() {
 			a[M.y][M.x] = ' ';
 			a[M.y - 2][M.x] = 98;
 			a[M.y - 1][M.x] = 15;
-			B.y--;
+			
 			M.y--;
 			return;
 		}
@@ -68,7 +70,7 @@ void Move() {
 			a[M.y][M.x] = ' ';
 			a[M.y][M.x - 2] = 98;
 			a[M.y][M.x - 1] = 15;
-			B.x--;
+			
 			M.x--;
 			return;
 		}
@@ -90,7 +92,7 @@ void Move() {
 			a[M.y][M.x] = ' ';
 			a[M.y + 2][M.x] = 98;
 			a[M.y + 1][M.x] = 15;
-			B.y++;
+			
 			M.y++;
 			return;
 		}
@@ -112,7 +114,7 @@ void Move() {
 			a[M.y][M.x] = ' ';
 			a[M.y][M.x + 2] = 98;
 			a[M.y][M.x + 1] = 15;
-			B.x++;
+			
 			M.x++;
 			return;
 		}
@@ -124,7 +126,14 @@ void Move() {
 	}
 }
 
-void ChuNhatDac(SDL_Renderer* renderer, int x1, int y1, int x2, int y2) {
+void ChuNhatDac(SDL_Renderer* renderer, int a, int b) {
+	
+	int x1 = (a - 1) * K,
+		y1 = (b - 1) * K,
+		x2 = a * K - 1,
+		y2 = b * K - 1;
+	
+	
 	for(int i = x1; i <= x2; i++) {
 		for(int j = y1; j <= y2; j++) {
 			SDL_RenderDrawPoint(renderer, i, j);
@@ -132,33 +141,78 @@ void ChuNhatDac(SDL_Renderer* renderer, int x1, int y1, int x2, int y2) {
 	}
 }
 
-void XoaChuNhatDac(SDL_Renderer* renderer, int x1, int y1, int x2, int y2) {
-	for(int i = x1; i <= x2; i++) {
-		for(int j = y1; j <= y2; j++) {
-			SDL_RenderDrawPoint(renderer, i, j);
-		}
+void ChuX (SDL_Renderer* renderer, int a, int b) {
+	int x1 = (a - 1) * K,
+		y1 = (b - 1) * K,
+		x2 = a * K - 1,
+		y2 = b * K - 1;
+		
+	for(int i = y1, j = x1; i <= y2; i++, j++) {
+		SDL_RenderDrawPoint(renderer, i, j);
 	}
+	for(int i = y1, j = x2; i <= y2; i++, j--) {
+		SDL_RenderDrawPoint(renderer, i, j);
+	}	
 }
 
 void moveGame() {
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	initSDL(window, renderer);
 	
-	m = 60;
-	n = 80;
-	
+	m = 600 / K;
+	n = 800 / K;
+	//xay map
 	for(int i = 0; i < m + 2; i++) {
 		for(int j = 0; j < n + 2; j++) {
 			if(i == 0 || i == m + 1 || j == 0 || j == n + 1) a[i][j] = 219;
 			else a[i][j] = ' ';
 		}
 	}
-	a[1][1] = 15;
-	a[2][2] = 98;
-	a[3][3] = 98;
+	
+	while(true) {
+		int i, j, c;
+		cin >> i >> j >> c;
+		if( c == -1 ) break;
+		if( c == 120 ) {
+			x[i][j] = c;
+			continue;
+		}
+		if( c == 15 ) {
+			a[i][j] = c;
+			M.y = i;
+			M.x = j;
+		}
+		a[i][j] = c;
+	}
+	
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	initSDL(window, renderer);
+	
+	
+	
 	//printArr();
-	while(SDL_WaitEvent(&e)) {
+	for(int i = 1; i <= m; i++) {
+		for(int j = 1; j <= n; j++) {
+			if( a[i][j] == char(15) ) {
+				SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
+				ChuNhatDac(renderer, j, i);
+			}
+			if( a[i][j] == char(98) ) {
+				SDL_SetRenderDrawColor(renderer, 255, 255, 0, 0);
+				ChuNhatDac(renderer, j, i);
+			}
+			if( a[i][j] == char(219) ) {
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+				ChuNhatDac(renderer, j, i);
+			}
+			if( x[i][j] == char(120) ) {
+				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+				ChuX(renderer, j, i);
+			}
+		}
+	}
+	SDL_RenderPresent(renderer);
+	
+	while( SDL_WaitEvent(&e) ) {
 		
 		if( e.type == SDL_KEYDOWN ) {
 			Move();
@@ -173,11 +227,15 @@ void moveGame() {
 			for(int j = 1; j <= n; j++) {
 				if( a[i][j] == char(15) ) {
 					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
-					ChuNhatDac(renderer, (j - 1) * 10, (i - 1) * 10, j * 10 - 1, i * 10 - 1);
+					ChuNhatDac(renderer, j, i);
 				}
 				if( a[i][j] == char(98) ) {
 					SDL_SetRenderDrawColor(renderer, 255, 255, 0, 0);
-					ChuNhatDac(renderer, (j - 1) * 10, (i - 1) * 10, j * 10 - 1, i * 10 - 1);
+					ChuNhatDac(renderer, j, i);
+				}
+				if( x[i][j] == 'x' ) {
+					SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+					ChuX(renderer, j, i);
 				}
 			}
 		}
@@ -187,8 +245,13 @@ void moveGame() {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 		for(int i = 1; i <= m; i++) {
 			for(int j = 1; j <= n; j++) {
-				if( a[i][j] != ' ' ) {
-					ChuNhatDac(renderer, (j - 1) * 10, (i - 1) * 10, j * 10 - 1, i * 10 - 1);
+				if( a[i][j] != ' ' && a[i][j] != char(219) ) {
+					ChuNhatDac(renderer, j, i);
+					if(x[i][j] == 'x') {
+						SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+						ChuX(renderer, j, i);
+						SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+					}
 				}
 			}
 		}
